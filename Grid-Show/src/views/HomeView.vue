90c979314c233c10eaba0426e9f222ba09f2f289
Nils van Gestel <nvangestel@yellowtail.nl>
2025-01-17 16:32:19 +0100

    testing stuff

--- Grid-Show/src/views/HomeView.vue
+++ Grid-Show/src/views/HomeView.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>Home</h1>
+  <Grid>
+    <div class="a">content</div>
+    <div class="b"></div>
+    <div class="c"></div>
+    <div class="d"></div>
+    <div class="e"></div>
+    <div class="f"></div>
+    <div class="g"></div>
+    <div class="h"></div
+  ></Grid>
 </template>
+
+<style>
+.a {
+  background-color: blue;
+}
+
+.b {
+  background-color: red;
+}
+
+.c {
+  background-color: blue;
+}
+</style>
