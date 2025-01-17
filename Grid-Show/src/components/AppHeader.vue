85773f3dda30084551fb66ec0dd9877c13219a3c
Nils van Gestel <nvangestel@yellowtail.nl>
2025-01-17 16:00:24 +0100

    fixed router, postcss and currentRoute Name

--- Grid-Show/src/components/AppHeader.vue
+++ Grid-Show/src/components/AppHeader.vue
@@ -1,22 +1,75 @@
 <template>
-  <header>
-    <div>
-      <nav class="navbar">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/one">One</RouterLink>
-        <RouterLink to="/two">Two</RouterLink>
-        <RouterLink to="/three">Three</RouterLink>
-        <RouterLink to="/four">Four</RouterLink>
-      </nav>
-    </div>
+  <header class="header" :class="{ 'is-active': isHovered }">
+    <nav class="header__navbar">
+      <ul>
+        <li><RouterLink to="/">Home</RouterLink></li>
+        <li><RouterLink to="/one">One</RouterLink></li>
+        <li><RouterLink to="/two">Two</RouterLink></li>
+        <li><RouterLink to="/three">Three</RouterLink></li>
+        <li><RouterLink to="/four">Four</RouterLink></li>
+      </ul>
+    </nav>
+    <h2 class="header__currentRoute">{{ route.name.toUpperCase() }}</h2>
   </header>
 </template>
 
-<style>
-.navbar {
-  display: flex;
-  padding: 1rem;
-  justify-content: space-around;
+<script setup>
+import { ref, onMounted, onUnmounted } from "vue";
+import { useRoute } from "vue-router";
+const route = useRoute();
+const isHovered = ref(false);
+
+const handleMouseMove = (event) => {
+  const screenHeight = window.innerHeight;
+  if (event.clientY < screenHeight * 0.1) {
+    isHovered.value = true;
+  } else {
+    isHovered.value = false;
+  }
+};
+
+onMounted(() => {
+  window.addEventListener("mousemove", handleMouseMove);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("mousemove", handleMouseMove);
+});
+</script>
+
+<style scoped>
+.header {
+  position: fixed;
+  top: -80px;
+  left: 0;
+  width: 100%;
   background-color: aliceblue;
+  transition: transform 0.3s ease-in-out;
+  z-index: 1000;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  &__navbar {
+    padding: 1rem;
+  }
+
+  &__navbar > ul {
+    display: flex;
+    list-style: none;
+  }
+
+  &__navbar > ul > li > a {
+    justify-content: space-between;
+    margin-right: 1rem;
+  }
+
+  &__currentRoute {
+    margin-right: 50%;
+  }
+}
+
+.header.is-active {
+  transform: translateY(80px);
 }
 </style>
